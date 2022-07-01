559b3584bcb127fd5f1cbe46ee9a2e40cbf6d5c3
Nam Tran <tnam5353@gmail.com>
2022-07-01 23:35:27 +0700

    fix movieList

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,14 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import HeroSlide from "../components/HeroSlide/HeroSlide";
 import { OutlineButton } from "../components/Button/Button";
 import MovieList from "../components/MovieList/MovieList";
 import { category, movieType, tvType } from "../api/tmdbApi";
 const Home = () => {
+  const navigate = useNavigate();
+  const handleOnClick = (type) => {
+    navigate(`/${type}`);
+  };
   return (
     <div>
       <HeroSlide />
@@ -11,7 +16,9 @@ const Home = () => {
         <div className="section mb-3">
           <div className="section__header mb-2">
             <h2>Trending moives</h2>
-            <OutlineButton>View more</OutlineButton>
+            <OutlineButton onClick={() => handleOnClick("movie")}>
+              View more
+            </OutlineButton>
           </div>
           <MovieList
             category={category.movie}
@@ -21,7 +28,9 @@ const Home = () => {
         <div className="section mb-3">
           <div className="section__header mb-2">
             <h2>Top rated moives</h2>
-            <OutlineButton>View more</OutlineButton>
+            <OutlineButton onClick={() => handleOnClick("movie")}>
+              View more
+            </OutlineButton>
           </div>
           <MovieList
             category={category.movie}
@@ -31,22 +40,20 @@ const Home = () => {
         <div className="section mb-3">
           <div className="section__header mb-2">
             <h2>Trending TV series</h2>
-            <OutlineButton>View more</OutlineButton>
+            <OutlineButton onClick={() => handleOnClick("tv")}>
+              View more
+            </OutlineButton>
           </div>
-          <MovieList
-            category={category.tv}
-            type={movieType.popular}
-          ></MovieList>
+          <MovieList category={category.tv} type={tvType.popular}></MovieList>
         </div>
         <div className="section mb-3">
           <div className="section__header mb-2">
             <h2>Top rated TV series</h2>
-            <OutlineButton>View more</OutlineButton>
+            <OutlineButton onClick={() => handleOnClick("tv")}>
+              View more
+            </OutlineButton>
           </div>
-          <MovieList
-            category={category.tv}
-            type={movieType.top_rated}
-          ></MovieList>
+          <MovieList category={category.tv} type={tvType.top_rated}></MovieList>
         </div>
       </div>
     </div>
